28aba75b417c81d0c15d12563326ed93d41820d5
Bastien Brunel <bastien.brunel@eleve.ensai.fr>
2025-10-03 10:24:41 +0200

    scrap cocktails

---
+++ src/utils/scrap_cocktails.py
@@ -0,0 +1,16 @@
+import json
+import urllib.request
+
+data = []
+i = ord('a')
+while i <= ord('z'):
+    with urllib.request.urlopen(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?f={chr(i)}") as url:
+        res = json.loads(url.read().decode())['drinks']
+        if res is not None:
+            for cocktail in res:
+                data.append(cocktail)
+    i += 1
+
+print(len(data))
+with open('coktails.json', 'w') as f:
+    f.write(json.dumps(data))
