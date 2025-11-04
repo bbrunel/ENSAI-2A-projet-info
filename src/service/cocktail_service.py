d970f8f3d13e7a93d9a8b88f91fcada6f07e45e6
id2722 <marceau.roze@eleve.ensai.fr>
2025-11-04 14:38:14 +0000

    modif sur fav cocktail et cocktail service du 04/11 à 15:38

--- src/service/cocktail_service.py
+++ src/service/cocktail_service.py
@@ -1,61 +1,59 @@
-
 from src.dao.cocktail import CocktailDAO
-from src.ingredient import Ingredient
+
+
 class CocktailService:
     """
     Classe service pour les cocktails
 
     """
-    def __init__(self, cocktail_dao : CocktailDAO) -> None:
+
+    def __init__(self, cocktail_dao: CocktailDAO) -> None:
         self.dao.cocktail = cocktail_dao
 
-
-
-
-    def verifier_cocktail(self, id_cocktail : int ) -> Cocktail:
+    def verifier_cocktail(self, id_cocktail: int) -> Cocktail:
         """Vérifie si un cocktail existe bel et bien déjà.
 
-        Paramètres 
+        Paramètres
         ----------
         id_cocktail : l'id du cocktail à vérifier/lire
-        
-        
+
+
         Retour
         ----------
         renvoie ErreurCocktailPasTrouvé: si le cocktail n'a pas été trouvé
-        retourne le cocktail dont on vérifie la présence 
-        """       
+        retourne le cocktail dont on vérifie la présence
+        """
         cocktail = self.dao.cocktail.lecture(id_cocktail)
         if cocktail is None:
             raise ErreurCocktailPasTrouvé(id_cocktail)
         return cocktail
 
-    
     def ingredient_cocktail(self, id_cocktail) -> list[Ingrédient]:
         """
         liste des ingrédients composant un cocktail demandé
 
         Paramètres
         ----------
-        id_cocktail : int 
+        id_cocktail : int
             l'id du cocktail dont on veut connaitre les ingrédients
-        
-        Retour 
+
+        Retour
         ----------
         Affiche ErreurCocktailPasTrouvé: si le cocktail n'a pas été trouvé
         Renvoie la liste des ingrédients composant le cocktail en question
         """
-        return 
+        return
+
     def lister_tous_cocktail() -> list[Cocktail]:
         """Lister l'ensemble des cocktails
 
-        Paramètres 
+        Paramètres
         ----------
-        
-        
+
+
         Retour
         ----------
         Affiche ErreurCocktailPasTrouvé: si aucun cocktail n'a été trouvé
-        Renvoie le cocktail dont on vérifie la présence 
+        Renvoie le cocktail dont on vérifie la présence
         """
-        return 
+        return
