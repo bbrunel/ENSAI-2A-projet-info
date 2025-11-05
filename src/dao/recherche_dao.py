4c82c7bcb1bc98ec05c6212bcf4080cefc872bb3
Bastien Brunel <bastien.brunel@eleve.ensai.fr>
2025-11-05 16:55:10 +0100

    erreurs recherche_dao

--- src/dao/recherche_dao.py
+++ src/dao/recherche_dao.py
@@ -2,20 +2,19 @@ import logging
 
 from business_object.cocktail import Cocktail
 from business_object.filtre_cocktail import FiltreCocktail
-from business_object.filtre_ingredient import FilreIngredient
+from business_object.filtre_ingredient import FiltreIngredient
 from business_object.ingredient import Ingredient
-
-from utils.singleton import Singleton
 from dao.db_connection import DBConnection
-from utils.log_decorator import log
+from utils.singleton import Singleton
+
 
 class RechercheDao(metaclass=Singleton):
     """
-        Classe DAO regroupant les méthodes permettant de chercher ingrédients et cocktails dans la
-        base de données
+    Classe DAO regroupant les méthodes permettant de chercher ingrédients et cocktails dans la
+    base de données
     """
 
-    def recherche_cocktail(self,filtre: FiltreCocktail) -> list[Cocktail]:
+    def recherche_cocktail(self, filtre: FiltreCocktail) -> list[Cocktail]:
         """Récupère dans la base de données la liste des cocktails satisfaisant un filtre
 
         Parameters
@@ -44,7 +43,7 @@ class RechercheDao(metaclass=Singleton):
                         params.append(filtre.iba)
                     if filtre.categorie is not None:
                         query += " AND category = %s"
-                        params.append(category)
+                        params.append(filtre.categorie)
                     if filtre.verre is not None:
                         query += "AND glass_type = %s"
                         params.append(verre)
@@ -56,15 +55,18 @@ class RechercheDao(metaclass=Singleton):
         liste_cocktails = []
         if res:
             for row in res:
-                cocktail = Cocktail(row['id_recipe'],
-                                    row['recipe_name'],
-                                    None,None,
-                                    row['category'],
-                                    row['iba_category'],
-                                    row['alcoholic'],
-                                    row['glass_type'],
-                                    row['instruction'],
-                                    row['cocktail_pic_url'])
+                cocktail = Cocktail(
+                    row["id_recipe"],
+                    row["recipe_name"],
+                    None,
+                    None,
+                    row["category"],
+                    row["iba_category"],
+                    row["alcoholic"],
+                    row["glass_type"],
+                    row["instruction"],
+                    row["cocktail_pic_url"],
+                )
                 liste_cocktails.append(cocktail)
         return liste_cocktails
 
@@ -88,20 +90,20 @@ class RechercheDao(metaclass=Singleton):
         try:
             with DBConnection.connection() as connection:
                 with connection.cursor() as cursor:
-            # La requete cherche dans la table composition les couple cocktail/ingredient
-            #dont l'ingredient n'est PAS dans la liste des ingredients possédés, ensuite
-            #on garde uniquement les cocktails dont le nombre d'occurence (donc le nombre
-            #d'ingredient supplementaire à utiliser) est inférieur à nb_manquants.
+                    # La requete cherche dans la table composition les couple cocktail/ingredient
+                    # dont l'ingredient n'est PAS dans la liste des ingredients possédés, ensuite
+                    # on garde uniquement les cocktails dont le nombre d'occurence (donc le nombre
+                    # d'ingredient supplementaire à utiliser) est inférieur à nb_manquants.
 
-                    query  = "SELECT c1.* as nb_missing FROM cocktails c1"
+                    query = "SELECT c1.* as nb_missing FROM cocktails c1"
                     query += " JOIN composition c2 ON c1.id_recipe = c2.id_recipe"
                     query += " WHERE NOT c2.id_ingredient IN %(liste_ing)s"
-                    query += " GROUP BY c1.id_recipe HAVING count(*) <= %(nb_max)s ORDER BY nb_missing;"
-                    cursor.execute(query,
-                                   {
-                                    "liste_ing": tuple(id_ingredients),
-                                    "nb_max": nb_manquants
-                                   })
+                    query += (
+                        " GROUP BY c1.id_recipe HAVING count(*) <= %(nb_max)s ORDER BY nb_missing;"
+                    )
+                    cursor.execute(
+                        query, {"liste_ing": tuple(id_ingredients), "nb_max": nb_manquants}
+                    )
                     res = cursor.fetchall()
         except Exception as e:
             logging.info(e)
@@ -109,15 +111,18 @@ class RechercheDao(metaclass=Singleton):
         liste_cocktails = []
         if res:
             for row in res:
-                cocktail = Cocktail(row['id_recipe'],
-                                    row['recipe_name'],
-                                    None,None,
-                                    row['category'],
-                                    row['iba_category'],
-                                    row['alcoholic'],
-                                    row['glass_type'],
-                                    row['instruction'],
-                                    row['cocktail_pic_url'])
+                cocktail = Cocktail(
+                    row["id_recipe"],
+                    row["recipe_name"],
+                    None,
+                    None,
+                    row["category"],
+                    row["iba_category"],
+                    row["alcoholic"],
+                    row["glass_type"],
+                    row["instruction"],
+                    row["cocktail_pic_url"],
+                )
                 liste_cocktails.append(cocktail)
         return liste_cocktails
 
@@ -142,16 +147,16 @@ class RechercheDao(metaclass=Singleton):
 
                     if filtre.nom is not None:
                         query += " AND similarity(LOWER(ingredient_name), LOWER(%(name)s)) > 0.5"
-                        params['name'] = filtre.nom
+                        params["name"] = filtre.nom
 
                     if filtre.alcoolise is not None:
                         query += " AND alcoholic = %(alcoolise)s"
-                        params['alcoolise'] = filtre.alcoolise
+                        params["alcoolise"] = filtre.alcoolise
 
                     if filtre.type_ing is not None:
                         query += " AND ingredient_type = %(type)s"
-                        params['type'] = filtre.type_ing
-                    cursor.execute(query,params)
+                        params["type"] = filtre.type_ing
+                    cursor.execute(query, params)
                     res = cursor.fetchall()
         except Exception as e:
             logging.info(e)
@@ -159,13 +164,14 @@ class RechercheDao(metaclass=Singleton):
         liste_ingredients = []
         if res:
             for row in res:
-                ingredient = Ingredient(row['id_ingredient'],
-                                        row['ingredient_name'],
-                                        row['description'],
-                                        row['ingredient_type'],
-                                        row['alcoholic'],
-                                        row['abv'])
+                ingredient = Ingredient(
+                    row["id_ingredient"],
+                    row["ingredient_name"],
+                    row["description"],
+                    row["ingredient_type"],
+                    row["alcoholic"],
+                    row["abv"],
+                )
                 liste_ingredients.append(ingredient)
         return liste_ingredients
-
 
