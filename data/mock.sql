6ce21eff8355d0510fd70fdb56c74a921ab2bbe2
Bastien Brunel <bastien.brunel@eleve.ensai.fr>
2025-11-05 15:05:17 +0100

    error mock.sql

--- data/mock.sql
+++ data/mock.sql
@@ -1,12 +1,12 @@
-INSERT INTO users (username, hashed_password) VALUES ("Gerald", "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
+INSERT INTO users (username, hashed_password) VALUES("Gerald", "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
 
-INSERT INTO users (username, hashed_password) VALUES ("Hector", "98c1eb4ee93476743763878fcb96a25fbc9a175074d64004779ecb5242f645e6");
+INSERT INTO users (username, hashed_password) VALUES("Hector", "98c1eb4ee93476743763878fcb96a25fbc9a175074d64004779ecb5242f645e6");
 
-INSERT INTO have (id_user, id_ingredient) VALUES (1,337);
-INSERT INTO have (id_user, id_ingredient) VALUES (1,305);
-INSERT INTO have (id_user, id_ingredient) VALUES (1,312);
+INSERT INTO have (id_user, id_ingredient) VALUES(1,337);
+INSERT INTO have (id_user, id_ingredient) VALUES(1,305);
+INSERT INTO have (id_user, id_ingredient) VALUES(1,312);
 
-INSERT INTO have (id_user, id_ingredient) VALUES (2,337);
-INSERT INTO have (id_user, id_ingredient) VALUES (2,305);
+INSERT INTO have (id_user, id_ingredient) VALUES(2,337);
+INSERT INTO have (id_user, id_ingredient) VALUES(2,305);
 
 
