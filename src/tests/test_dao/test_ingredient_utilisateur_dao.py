23d68c5274b44c457c12aead5e53421c8c271fe4
jmalet <julie.malet@eleve.ensai.fr>
2025-11-06 14:15:09 +0000

    effacement commentaire

--- src/tests/test_dao/test_ingredient_utilisateur_dao.py
+++ src/tests/test_dao/test_ingredient_utilisateur_dao.py
@@ -11,26 +11,8 @@ from dao.ingredient_utilisateur_dao import IngredientUtilisateurDao
 
 from business_object.ingredient import Ingredient
 
-# Solution de fortune ?
 
 from dao.db_connection import DBConnection
-
-# with DBConnection().connection as connection:
-#     with connection.cursor() as cursor:
-#         cursor.execute(
-#             """
-#             INSERT INTO users(username, hashed_password) VALUES('Gerald', '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824');
-
-#             INSERT INTO users(username, hashed_password) VALUES('Hector', '98c1eb4ee93476743763878fcb96a25fbc9a175074d64004779ecb5242f645e6');
-
-#             INSERT INTO have(id_user, id_ingredient) VALUES(1,337);
-#             INSERT INTO have(id_user, id_ingredient) VALUES(1,305);
-#             INSERT INTO have(id_user, id_ingredient) VALUES(1,312);
-
-#             INSERT INTO have(id_user, id_ingredient) VALUES(2,337);
-#             INSERT INTO have(id_user, id_ingredient) VALUES(2,305);
-#             """
-#         )
 
 
 # @pytest.fixture(scope="session", autouse=True)
