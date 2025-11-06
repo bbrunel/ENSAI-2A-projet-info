972bd6b9fbf02605b396a7dd14dd9425fb3e0abc
valentindmx <valentin.demouveaux@eleve.ensai.fr>
2025-11-06 11:24:02 +0000

    feat: ajout affichage lisible pour objet business cocktails

--- src/business_object/cocktail.py
+++ src/business_object/cocktail.py
@@ -50,3 +50,8 @@ class Cocktail():
         self.verre = verre 
         self.instructions = instructions
         self.url_image = url_image
+
+    def __str__(self):
+        """Permet de voir les caractéristiques du cocktail"""
+        return f'{self.id}, {self.nom}, {self.nom_alt}, {self.alcoolise}, {self.tags}, {self.iba}, \
+{self.categorie}, {self.verre}, {self.instructions}'
