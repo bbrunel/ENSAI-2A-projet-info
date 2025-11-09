7a8495b2f99f21bc1f42f45407f03979cd3b8ef2
bbrunel <bastien.brunel@eleve.ensai.fr>
2025-11-09 17:16:11 +0000

    endpoint liste_faisables

--- src/api/routers/auth.py
+++ src/api/routers/auth.py
@@ -1,85 +1,13 @@
-from datetime import datetime, timedelta, timezone
+from datetime import timedelta
 from typing import Annotated
 
-import jwt
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
-from pwdlib import PasswordHash
-from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordRequestForm
 
+from api.securite import *
 from business_object.utilisateur import Utilisateur
-from service.utilisateur_service import UtilisateurService
 
-SECRET_KEY = "e99fdc95d8416935051d4ac4bf7914cc4de503d9d813074ec4174e2e6fe46f25"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-password_hash = PasswordHash.recommended()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter()
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-
-def verify_password(plain_password, hashed_password):
-    return password_hash.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return password_hash.hash(password)
-
-
-def authenticate_user(username: str, password: str):
-    user = UtilisateurService.trouver_par_nom(username)
-    if not user:
-        return False
-    if not verify_password(password, user.mdp):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
-    user = UtilisateurService.trouver_par_nom(token_data.username)
-    if user is None:
-        raise create_access_token
-    return user
-
-
-@router.get("/user/me")
-def user_me(current_user: Annotated[Utilisateur, Depends(get_current_user)]):
-    return current_user
 
 
 @router.post("/token")
@@ -97,3 +25,8 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     )
 
     return Token(access_token=access_token, token_type="bearer")
+
+
+@router.get("/user/me")
+def user_me(current_user: Annotated[Utilisateur, Depends(get_current_user)]):
+    return current_user
--- src/api/routers/recherche.py
+++ src/api/routers/recherche.py
@@ -1,21 +1,24 @@
 from typing import Annotated
+
 from fastapi import APIRouter, Query
 
-
+from api.securite import *
 from business_object.filtre_cocktail import FiltreCocktail
-from business_object.filtre_ingredient import FiltreIngredient
+from business_object.utilisateur import Utilisateur
 from service.recherche_service import RechercheService
 
 router = APIRouter()
 
 recherche_service = RechercheService()
 
-@router.get("/recherche_cocktail/{nom_cocktail}", tags=["Cocktails","Recherche"])
+
+@router.get("/recherche_cocktail/{nom_cocktail}", tags=["Cocktails", "Recherche"])
 async def recherche_cocktail(nom_cocktail: str):
     """Recherche de cocktail"""
-    filtre = FiltreCocktail(nom = nom_cocktail)
+    filtre = FiltreCocktail(nom=nom_cocktail)
     cocktails = recherche_service.recherche_cocktail(filtre)
     return cocktails
+
 
 @router.get("/recherche_filtre_cocktail", tags=["Cocktails", "Recherche"])
 def recherche_filtre_cocktail(filtre: Annotated[FiltreCocktail, Query()]):
@@ -23,3 +26,8 @@ def recherche_filtre_cocktail(filtre: Annotated[FiltreCocktail, Query()]):
     return cocktails
 
 
+@router.get("/liste_faisables/{nb_manquants}", tags=["Cocktails"])
+def liste_cocktail_quasifaisables(
+    current_user: Annotated[Utilisateur, Depends(get_current_user)], nb_manquants: int
+):
+    return recherche_service.liste_cocktails_faisables(current_user, nb_manquants)
