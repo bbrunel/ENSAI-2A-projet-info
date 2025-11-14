6dc67a03c911bd17c381796dd35859fb87357d68
bbrunel <bastien.brunel@eleve.ensai.fr>
2025-11-14 15:34:16 +0000

    favoris endpoints

--- src/api/routers/favoris.py
+++ src/api/routers/favoris.py
@@ -15,14 +15,19 @@ def ajout_favori(
     current_user: Annotated[Utilisateur, Depends(get_current_user)],
     id_cocktail: Annotated[int, Query()],
 ):
-    try:
-        id_fav = favoris_service.aj_fav_cocktail(current_user.id, id_cocktail)
-    except ValueError as e:
-        return e
-    return id_fav
+    cocktail = favoris_service.aj_fav_cocktail(current_user.id, id_cocktail)
+    return cocktail
 
 
 @router.get("/liste_favoris", tags=["Favoris"])
 def liste_favoris(current_user: Annotated[Utilisateur, Depends(get_current_user)]):
     favs = favoris_service.list_all_fav_cocktails(current_user.id)
     return favs
+
+
+@router.delete("/supprimer_favori", tags=["Favoris"])
+def supprimer_favori(
+    current_user: Annotated[Utilisateur, Depends(get_current_user)],
+    id_cocktail: Annotated[int, Query()],
+):
+    favoris_service.suppr_fav_cocktail(current_user.id, id_cocktail)
