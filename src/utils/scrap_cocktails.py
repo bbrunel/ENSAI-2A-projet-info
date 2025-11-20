f6311ab8bc504e732f5b9b934666b6819b984ef4
bbrunel <bastien.brunel@eleve.ensai.fr>
2025-11-20 12:18:22 +0000

    delete files from teacher template

--- src/utils/scrap_cocktails.py
+++ src/utils/scrap_cocktails.py
@@ -2,15 +2,17 @@ import json
 import urllib.request
 
 data = []
-i = ord('a')
-while i <= ord('z'):
-    with urllib.request.urlopen(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?f={chr(i)}") as url:
-        res = json.loads(url.read().decode())['drinks']
+i = ord("a")
+while i <= ord("z"):
+    with urllib.request.urlopen(
+        f"https://www.thecocktaildb.com/api/json/v1/1/search.php?f={chr(i)}"
+    ) as url:
+        res = json.loads(url.read().decode())["drinks"]
         if res is not None:
             for cocktail in res:
                 data.append(cocktail)
     i += 1
 
 print(len(data))
-with open('cocktails.json', 'w') as f:
+with open("cocktails.json", "w") as f:
     f.write(json.dumps(data))
--- src/utils/scrap_ingredients.py
+++ src/utils/scrap_ingredients.py
@@ -6,16 +6,18 @@ data = []
 i = 1
 while i <= 616:
     try:
-        with urllib.request.urlopen(f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid={i}") as url:
+        with urllib.request.urlopen(
+            f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid={i}"
+        ) as url:
             res = json.loads(url.read().decode())
-            if res is not None and res['ingredients'] is not None:
-                for ingredient in res['ingredients']:
+            if res is not None and res["ingredients"] is not None:
+                for ingredient in res["ingredients"]:
                     data.append(ingredient)
         i += 1
-    except:
-        print(i) # homemade pogress bar
+    except Exception:
+        print(i)  # homemade pogress bar
         time.sleep(10)
 
 print(len(data))
-with open('ingredients.json', 'w') as f:
+with open("ingredients.json", "w") as f:
     f.write(json.dumps(data))
